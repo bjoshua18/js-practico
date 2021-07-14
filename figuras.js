2b598b7fc6cb720f693e7b123ec7b37c783d3b37
Byron Bustamante <byronbl.12@gmail.com>
2021-07-14 13:53:23 +0100

    Creación de interfaz de usuario

--- figuras.js
+++ figuras.js
@@ -1,47 +1,78 @@
 // Cuadrado
-console.group('Cuadrado');
-
-const ladoCuadrado = 5;
-
 const perimetroCuadrado = lado => lado * 4;
 const areaCuadrado = lado => lado * lado;
 
-console.log(`Los lados del cuadrado miden: ${ladoCuadrado}cm`);
-console.log(`El perímetro del cuadrado mide: ${perimetroCuadrado(ladoCuadrado)}cm`);
-console.log(`El área del cuadrado mide: ${areaCuadrado(ladoCuadrado)}cm²`);
-
-console.groupEnd();
-
 // Triángulo
-console.group('Triángulo');
-
-const ladoTriangulo1 = 6;
-const ladoTriangulo2 = 6;
-const baseTriangulo = 4;
-const alturaTriangulo = 5.5;
-
+const alturaTriangulo = (area, base) => (area * 2) / base;
 const perimetroTriangulo = (lado1, lado2, base) => lado1 + lado2 + base;
-const areaTriangulo = (base, altura) => (base * altura) / 2;
-
-console.log(`Los lados del triángulo miden: ${ladoTriangulo1}cm, ${ladoTriangulo2}cm, ${baseTriangulo}cm`);
-console.log(`La altura del triángulo es de: ${alturaTriangulo}cm`);
-console.log(`El perímetro del triángulo es: ${perimetroTriangulo(ladoTriangulo1, ladoTriangulo2, baseTriangulo)}cm`);
-console.log(`El área del triángulo es: ${areaTriangulo(baseTriangulo, alturaTriangulo)}cm²`)
-
-console.groupEnd();
+const areaTriangulo = (lado1, lado2, base) => {
+  const s = (lado1 + lado2 + base) / 2;
+  return Math.sqrt(s * (s - lado1) * (s - lado2) * (s - base));
+}
+const alturaTrianguloIsoscele = (lado1, lado2, base) => {
+  if (lado1 !== lado2) return false;
+  return Math.sqrt((lado1 ** 2) - ((base ** 2) / 4));
+}
 
 // Círculo
-console.group('Círculo');
-
-const radioCirculo = 4;
-
 const diametroCirculo = radio => radio * 2;
 const perimetroCirculo = radio => diametroCirculo(radio) * Math.PI;
 const areaCirculo = radio => radio * radio * Math.PI;
 
-console.log(`El radio del círculo es: ${radioCirculo}cm`);
-console.log(`El diámetro del círculo es: ${diametroCirculo(radioCirculo)}cm`);
-console.log(`El perímetro del círculo es: ${perimetroCirculo(radioCirculo)}cm`);
-console.log(`El área del círculo es: ${areaCirculo(radioCirculo)}cm²`);
+// Funciones onclick
+const calcularPerimetroCuadrado = () => setMessageResult(
+        'perimetroCuadrado',
+        `El perímetro es ${perimetroCuadrado(getLadoCuadrado())}`
+);
 
-console.groupEnd();
+const calcularAreaCuadrado = () => setMessageResult(
+        'areaCuadrado',
+        `El área es ${areaCuadrado(getLadoCuadrado())}`
+);
+
+const calcularAlturaTriangulo = () => {
+  const lados = getLadosTriangulo();
+  setMessageResult(
+          'alturaTriangulo',
+          `La altura es ${alturaTriangulo(areaTriangulo(...lados), lados[2])}`
+  )
+}
+
+const calcularPerimetroTriangulo = () => setMessageResult(
+        'perimetroTriangulo',
+        `El perímetro es ${perimetroTriangulo(...getLadosTriangulo())}`
+);
+
+const calcularAreaTriangulo = () => setMessageResult(
+          'areaTriangulo',
+          `El área es ${areaTriangulo(...getLadosTriangulo())}`
+);
+
+const calcularPerimetroCirculo = () => setMessageResult(
+        'perimetroCirculo',
+        `El perímetro es ${perimetroCirculo(getRadio())}`
+);
+
+const calcularAreaCirculo = () => setMessageResult(
+        'areaCirculo',
+        `El área es ${areaCirculo(getRadio())}`
+);
+
+// Funciones auxiliares
+
+function getLadoCuadrado() {
+  return +(document.getElementById('InputCuadrado').value);
+}
+
+function getLadosTriangulo() {
+  const lados = document.getElementById('InputTriangulo').value.split(',');
+  return [+lados[0], +lados[1], +lados[2]];
+}
+
+function getRadio() {
+  return +(document.getElementById('InputCirculo').value);
+}
+
+function setMessageResult(id, message) {
+  document.getElementById(id).innerText = message;
+}
